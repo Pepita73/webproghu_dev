30d4ef722af7402dee097d5c6059a40b353adc18
Pepita <pepita@pepitaweb.hu>
2018-08-04 22:46:23 +0200

    #13 remove drupal's append from our settings

--- src/config/settings.php
+++ src/config/settings.php
@@ -794,15 +794,3 @@ $settings['entity_update_batch_size'] = 50;
 # if (file_exists($app_root . '/' . $site_path . '/settings.local.php')) {
 #   include $app_root . '/' . $site_path . '/settings.local.php';
 # }
-$config_directories['sync'] = '../config/sync';
-$databases['default']['default'] = array (
-  'database' => 'webproghu',
-  'username' => 'root',
-  'password' => 'admin',
-  'prefix' => '',
-  'host' => 'db',
-  'port' => '3306',
-  'namespace' => 'Drupal\\Core\\Database\\Driver\\mysql',
-  'driver' => 'mysql',
-);
-$settings['install_profile'] = 'standard';
